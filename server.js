aff0808283cb02dc18e33907291a17a059360f5d
cgonz <christoph.gonz@gmail.com>
2023-05-05 03:52:12 -0700

    added get and post for api/notes

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ app.use(express.static("public"));
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public")));
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public", "notes.html"))
 );
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+);
 
+app.get("/api/notes", (req, res) => console.log("1"));
+app.post("/api/notes", (req, res) => console.log("1"));
 app.listen(PORT, () => console.log("App listening at " + PORT));
