01f440a6ab469b8b7824e106fcb1406b4c97ee40
cgonz <christoph.gonz@gmail.com>
2023-05-08 16:27:04 -0700

    adding comments and updating the readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # c11-note-taker
+
+## Description
+
+This is an application that allows the user to view and create notes on a hosted website. This is a helpful organizational tool that can allow you to keep track of important things
+to help your efficiency. It was helpful to create this application in order to familiarize myself with express and understand how to handle requests.
+
+## Installation
+
+It is required to use this application with node and npm. The required dependencies are Express and random-id which will be installed by using npm.
+With the files in the desired directory, you can install the above dependencies through an "npm install"
+
+## Usage
+
+It is required to host this application online, through a hosting the server online or through localHost at your desired port.
+After navigating to the site and clicking the getting started button, you can create the notes by filling in the relevant sections.
+You can then save the note by pressing the save icon in the uppper right corner. You can select the note you would like to see by pressing the note on the left hand side.
+You can also delete notes that you no longer need by pressing the trash icon.
--- server.js
+++ server.js
@@ -1,54 +1,51 @@
+// require the dependencies
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const dataset = require("./db/db.json");
 const randomId = require("random-id");
 
+// setting up the port and express
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// setting up the paths for the files the customers can interact with
 app.use(express.static("public"));
 
+// setting up json with express
 app.use(express.json());
 
+// route to homepage
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public")));
+
+// path to the notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public", "notes.html"))
 );
 
+// get request for displaying the notes
 app.get("/api/notes", (req, res) => res.json(dataset));
+
+// post request to update the notes
 app.post("/api/notes", (req, res) => {
+  // get the request and add a random id
   const newNote = {
     title: req.body.title,
     text: req.body.text,
     id: randomId(4, "aA0"),
   };
 
+  // variable for the data that we will use
   let storedNotes;
+
+  // retrieving the data from db.json
   fs.readFile(`./db/db.json`, "utf8", function (error, data) {
+    // assigning storedNotes the db data as an array
     storedNotes = JSON.parse(data);
+    // pushing the new note onto the variable
     storedNotes.push(newNote);
-    const noteString = JSON.stringify(storedNotes);
-    // Write the string to a file
-    fs.writeFile(`./db/db.json`, noteString, (err) =>
-      err ? console.error(err) : console.log("Success")
-    );
-    res.json(dataset);
-  });
-});
-app.delete("/api/notes/:id", (req, res) => {
-  const deleteId = req.params.id;
-  fs.readFile(`./db/db.json`, "utf8", function (error, data) {
-    let storedNotes = JSON.parse(data);
-    let toDelete;
-    let newNotes;
-    for (let i = 0; i < storedNotes.length; i++) {
-      const element = storedNotes[i].id;
-      if (element == deleteId) {
-        toDelete = i;
-      }
-    }
-    storedNotes.splice(toDelete, 1);
+
+    // turning the array into a json string
     const noteString = JSON.stringify(storedNotes);
     // Write the string to a file
     fs.writeFile(`./db/db.json`, noteString, (err) =>
@@ -58,7 +55,40 @@ app.delete("/api/notes/:id", (req, res) => {
   });
 });
 
+// delete request to delete a note
+app.delete("/api/notes/:id", (req, res) => {
+  // get the id that for the note that is to be deleted
+  const deleteId = req.params.id;
+  // retrieveing the data from db.json
+  fs.readFile(`./db/db.json`, "utf8", function (error, data) {
+    // the json data as an array
+    let storedNotes = JSON.parse(data);
+    // the index of the note that is to be deleted
+    let toDelete;
+    for (let i = 0; i < storedNotes.length; i++) {
+      const element = storedNotes[i].id;
+      // if the element has the id to delete
+      if (element == deleteId) {
+        // get the index
+        toDelete = i;
+      }
+    }
+    // remove the item from the array
+    storedNotes.splice(toDelete, 1);
+    // turning the array into a json string
+    const noteString = JSON.stringify(storedNotes);
+    // Write the string to a file
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
+      err ? console.error(err) : console.log("Success")
+    );
+    res.json(dataset);
+  });
+});
+
+// wildcard if any url is requested that isn't a route
 app.get("/*", (req, res) =>
   res.sendFile(path.join(__dirname, "public", "index.html"))
 );
+
+// creating the server
 app.listen(PORT, () => console.log("App listening at " + PORT));
